5235b677df1aa27d8d29ee1517b50f34f19c08b6
LILIYA SADYKOVA <sadykova.lilya@gmail.com>
2022-08-06 21:39:40 -0400

    committing my changes

--- HelloIDEA/src/Main.java
+++ HelloIDEA/src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, IDEA");
+        System.out.println("Hello, It's Lilya!!");
         System.out.println("I'm a student");
     }
 }
